d97c3a9da7492acfb33400dcb673aeadb2dceb7e
Lukas Nagel <nagellukas04@outlook.com>
2022-07-09 11:35:43 +0200

    updated serrver type

--- Hetzner/lna-dev-api.tf
+++ Hetzner/lna-dev-api.tf
@@ -1,6 +1,6 @@
 resource "hcloud_server" "api_lna-dev_com" {
   name         = "api.lna-dev.com.TF"
-  server_type  = "cx11"
+  server_type  = "cpx11"
   image        = "ubuntu-20.04"
   location     = "nbg1"
   firewall_ids = [hcloud_firewall.API-Firewall.id]
