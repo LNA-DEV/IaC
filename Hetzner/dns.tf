a0506307685f05de8d670aba008b672637269dc1
Lukas Nagel <lukas.nagel04@protonmail.com>
2022-07-14 06:42:24 +0200

    added kubeNode dns test record

--- Hetzner/dns.tf
+++ Hetzner/dns.tf
@@ -10,6 +10,14 @@ resource "hetznerdns_record" "empty" {
     ttl= 120
 }
 
+resource "hetznerdns_record" "empty2" {
+    zone_id = data.hetznerdns_zone.dns_zone.id
+    name = "*"
+    value = hcloud_server.kubeMaster.ipv4_address
+    type = "A"
+    ttl= 120
+}
+
 resource "hetznerdns_record" "kubectl" {
     zone_id = data.hetznerdns_zone.dns_zone.id
     name = "kubectl"
@@ -17,3 +25,12 @@ resource "hetznerdns_record" "kubectl" {
     type = "A"
     ttl= 120
 }
+
+# # TODO
+# resource "hetznerdns_record" "kubeNode" {
+#     zone_id = data.hetznerdns_zone.dns_zone.id
+#     name = "kubeNode" + hcloud_server.kubeNode[count.index]
+#     value = hcloud_server.kubeNode[count.index].ipv4_address
+#     type = "A"
+#     ttl= 120
+# }
