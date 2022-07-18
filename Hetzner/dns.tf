7e6e24f82925bb89103d47957c3fb6b9cc8232a4
Lukas Nagel <lukas.nagel04@protonmail.com>
2022-07-18 08:34:44 +0200

    added dns records for mailservers

--- Hetzner/dns.tf
+++ Hetzner/dns.tf
@@ -1,8 +1,11 @@
+# General
 data "hetznerdns_zone" "dns_zone" {
   name = "lna-dev.net"
 }
 
-resource "hetznerdns_record" "empty" {
+
+# Kubernetes
+resource "hetznerdns_record" "lna-dev_net" {
   zone_id = data.hetznerdns_zone.dns_zone.id
   name    = "@"
   value   = hcloud_server.kubeMaster.ipv4_address
@@ -10,7 +13,7 @@ resource "hetznerdns_record" "empty" {
   ttl     = 120
 }
 
-resource "hetznerdns_record" "empty2" {
+resource "hetznerdns_record" "lna-dev_net2" {
   zone_id = data.hetznerdns_zone.dns_zone.id
   name    = "*"
   value   = hcloud_server.kubeMaster.ipv4_address
@@ -26,11 +29,21 @@ resource "hetznerdns_record" "kubectl" {
   ttl     = 120
 }
 
-# # TODO
-# resource "hetznerdns_record" "kubeNode" {
-#     zone_id = data.hetznerdns_zone.dns_zone.id
-#     name = "kubeNode" + hcloud_server.kubeNode[count.index]
-#     value = hcloud_server.kubeNode[count.index].ipv4_address
-#     type = "A"
-#     ttl= 120
+
+# Mail
+# Deactivated because the infrastructure descibed in the terraform scripts does not run permanently yet and the entrys are already needed
+# resource "hetznerdns_record" "mail1" {
+#   zone_id = data.hetznerdns_zone.dns_zone.id
+#   name    = "@"
+#   value   = "mx00.ionos.de."
+#   type    = "MX"
+#   ttl     = 120
+# }
+
+# resource "hetznerdns_record" "mail2" {
+#   zone_id = data.hetznerdns_zone.dns_zone.id
+#   name    = "@"
+#   value   = "mx01.ionos.de."
+#   type    = "MX"
+#   ttl     = 120
 # }
