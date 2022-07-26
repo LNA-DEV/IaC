877c864bffd26a7d01b2ef403e880934577ab123
Lukas Nagel <lukas@lna-dev.net>
2022-07-26 11:56:44 +0200

    remvoed kubernetes dns entrys

--- Hetzner/dns.tf
+++ Hetzner/dns.tf
@@ -4,33 +4,6 @@ data "hetznerdns_zone" "dns_zone" {
 }
 
 
-# TODO Use module output vars
-# # Kubernetes
-# resource "hetznerdns_record" "lna-dev_net" {
-#   zone_id = data.hetznerdns_zone.dns_zone.id
-#   name    = "@"
-#   value   = hcloud_server.kubeMaster.ipv4_address
-#   type    = "A"
-#   ttl     = 120
-# }
-
-# resource "hetznerdns_record" "lna-dev_net2" {
-#   zone_id = data.hetznerdns_zone.dns_zone.id
-#   name    = "*"
-#   value   = hcloud_server.kubeMaster.ipv4_address
-#   type    = "A"
-#   ttl     = 120
-# }
-
-# resource "hetznerdns_record" "kubectl" {
-#   zone_id = data.hetznerdns_zone.dns_zone.id
-#   name    = "kubectl"
-#   value   = hcloud_server.kubeMaster.ipv4_address
-#   type    = "A"
-#   ttl     = 120
-# }
-
-
 # Mail
 resource "hetznerdns_record" "mail1" {
   zone_id = data.hetznerdns_zone.dns_zone.id
