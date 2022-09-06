d2f5d8e9bce47dd34f4fa3861c7186303996b9d4
Lukas Nagel <lukas@lna-dev.net>
2022-09-06 10:34:43 -0600

    updated dns records

--- Hetzner/dns.tf
+++ Hetzner/dns.tf
@@ -153,6 +153,30 @@ resource "hetznerdns_record" "prüfungsvorbereitung" {
   ttl     = 86400
 }
 
+resource "hetznerdns_record" "prüfungsvorbereitung2" {
+  zone_id = data.hetznerdns_zone.prüfungsvorbereitung.id
+  name    = "@"
+  value   = "185.199.108.153"
+  type    = "A"
+  ttl     = 86400
+}
+
+resource "hetznerdns_record" "prüfungsvorbereitung3" {
+  zone_id = data.hetznerdns_zone.prüfungsvorbereitung.id
+  name    = "@"
+  value   = "185.199.110.153"
+  type    = "A"
+  ttl     = 86400
+}
+
+resource "hetznerdns_record" "prüfungsvorbereitung4" {
+  zone_id = data.hetznerdns_zone.prüfungsvorbereitung.id
+  name    = "@"
+  value   = "185.199.111.153"
+  type    = "A"
+  ttl     = 86400
+}
+
 # fachinformatiker-prüfungsvorbereitung.de
 resource "hetznerdns_record" "prüfungsvorbereitungWWW" {
   zone_id = data.hetznerdns_zone.prüfungsvorbereitung.id
