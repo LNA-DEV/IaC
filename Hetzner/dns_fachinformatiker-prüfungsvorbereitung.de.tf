f28d3aeac213d2f049abed1d3a0fabe5c08b2a6b
Lukas Nagel <lukas@lna-dev.net>
2022-09-27 07:13:25 +0200

    Updated fi pv records

--- Hetzner/dns_fachinformatiker-prüfungsvorbereitung.de.tf
+++ Hetzner/dns_fachinformatiker-prüfungsvorbereitung.de.tf
@@ -2,45 +2,20 @@ data "hetznerdns_zone" "prüfungsvorbereitung" {
   name = "xn--fachinformatiker-prfungsvorbereitung-w0d.de"
 }
 
-# fachinformatiker-prüfungsvorbereitung.de
 resource "hetznerdns_record" "prüfungsvorbereitung" {
   zone_id = data.hetznerdns_zone.prüfungsvorbereitung.id
   name    = "@"
-  value   = "185.199.109.153"
+  value   = module.kube-hetzner.load_balancer_public_ipv4
   type    = "A"
   ttl     = 86400
 }
 
-resource "hetznerdns_record" "prüfungsvorbereitung2" {
-  zone_id = data.hetznerdns_zone.prüfungsvorbereitung.id
-  name    = "@"
-  value   = "185.199.108.153"
-  type    = "A"
-  ttl     = 86400
-}
 
-resource "hetznerdns_record" "prüfungsvorbereitung3" {
-  zone_id = data.hetznerdns_zone.prüfungsvorbereitung.id
-  name    = "@"
-  value   = "185.199.110.153"
-  type    = "A"
-  ttl     = 86400
-}
-
-resource "hetznerdns_record" "prüfungsvorbereitung4" {
-  zone_id = data.hetznerdns_zone.prüfungsvorbereitung.id
-  name    = "@"
-  value   = "185.199.111.153"
-  type    = "A"
-  ttl     = 86400
-}
-
-# fachinformatiker-prüfungsvorbereitung.de
 resource "hetznerdns_record" "prüfungsvorbereitungWWW" {
   zone_id = data.hetznerdns_zone.prüfungsvorbereitung.id
   name    = "www"
-  value   = "lna-dev.github.io."
-  type    = "CNAME"
+  value   = module.kube-hetzner.load_balancer_public_ipv4
+  type    = "A"
   ttl     = 86400
 }
 
