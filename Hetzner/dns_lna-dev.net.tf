649948a3897d393608b0e79f2f18af4f1e5b873d
Lukas Nagel <lukas@lna-dev.net>
2022-10-01 21:02:54 +0200

    Removed DNS entries

--- Hetzner/dns_lna-dev.net.tf
+++ Hetzner/dns_lna-dev.net.tf
@@ -3,7 +3,6 @@ data "hetznerdns_zone" "dns_zone" {
   name = "lna-dev.net"
 }
 
-
 # Mail
 resource "hetznerdns_record" "mail1" {
   zone_id = data.hetznerdns_zone.dns_zone.id
@@ -21,46 +20,10 @@ resource "hetznerdns_record" "mail2" {
   ttl     = 120
 }
 
-# me.lna-dev.net
-resource "hetznerdns_record" "me" {
-  zone_id = data.hetznerdns_zone.dns_zone.id
-  name    = "me"
-  value   = "lna-dev.github.io."
-  type    = "CNAME"
-  ttl     = 120
-}
-
-# iac.lna-dev.net
-resource "hetznerdns_record" "IaC" {
-  zone_id = data.hetznerdns_zone.dns_zone.id
-  name    = "iac"
-  value   = "lna-dev.github.io."
-  type    = "CNAME"
-  ttl     = 120
-}
-
-# thk.lna-dev.net
-resource "hetznerdns_record" "thk" {
-  zone_id = data.hetznerdns_zone.dns_zone.id
-  name    = "thk"
-  value   = "lna-dev.github.io."
-  type    = "CNAME"
-  ttl     = 120
-}
-
 # api.lna-dev.net
 resource "hetznerdns_record" "api" {
   zone_id = data.hetznerdns_zone.dns_zone.id
   name    = "api"
-  value   = module.kube-hetzner.load_balancer_public_ipv4
-  type    = "A"
-  ttl     = 120
-}
-
-# mongo.lna-dev.net
-resource "hetznerdns_record" "mongo" {
-  zone_id = data.hetznerdns_zone.dns_zone.id
-  name    = "mongo"
   value   = module.kube-hetzner.load_balancer_public_ipv4
   type    = "A"
   ttl     = 120
@@ -76,31 +39,11 @@ resource "hetznerdns_record" "ce" {
   ttl     = 120
 }
 
-# Privacy Tools
-# privacy.lna-dev.net
-resource "hetznerdns_record" "privacy" {
-  zone_id = data.hetznerdns_zone.dns_zone.id
-  name    = "privacy"
-  value   = "lna-dev.github.io."
-  type    = "CNAME"
-  ttl     = 120
-}
-
 # LNA-Photo
 # photo.lna-dev.net
 resource "hetznerdns_record" "photo" {
   zone_id = data.hetznerdns_zone.dns_zone.id
   name    = "photo"
-  value   = "lna-dev.github.io."
-  type    = "CNAME"
-  ttl     = 120
-}
-
-# LNA-Blog
-# blog.lna-dev.net
-resource "hetznerdns_record" "blog" {
-  zone_id = data.hetznerdns_zone.dns_zone.id
-  name    = "blog"
   value   = "lna-dev.github.io."
   type    = "CNAME"
   ttl     = 120
