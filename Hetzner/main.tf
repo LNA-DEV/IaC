46f8bab1f3fd0ba4717ded0c54c79895a3b4d095
Lukas Nagel <lukas@lna-dev.net>
2022-11-30 17:42:45 +0100

    Updated Provider Version

--- Hetzner/main.tf
+++ Hetzner/main.tf
@@ -9,7 +9,7 @@ terraform {
   required_providers {
     hcloud = {
       source  = "hetznercloud/hcloud"
-      version = "1.35.1"
+      version = "1.35.2"
     }
     hetznerdns = {
       source  = "timohirt/hetznerdns"
