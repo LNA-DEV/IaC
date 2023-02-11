c0b31c13ac6bffda8cfd954fcfe3db12a6c5b91b
Lukas Nagel <lukas@lna-dev.net>
2023-02-11 23:55:13 +0100

    🧱 Added GitHub Verify

---
+++ Hetzner/dns_fedodo.org.tf
@@ -0,0 +1,12 @@
+data "hetznerdns_zone" "fedodo_org" {
+  name = "fedodo.org"
+}
+
+# Github Organisation Verification
+resource "hetznerdns_record" "fedodoGitHubVerify" {
+  zone_id = data.hetznerdns_zone.fedodo_org.id
+  name    = "@"
+  value   = "604c443ac7"
+  type    = "TXT"
+  ttl     = 86400
+}
