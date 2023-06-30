47a37cda278ff00b12399350330e50866ded5977
Lukas Nagel <lukas@lna-dev.net>
2023-06-30 07:09:27 +0200

    ⚗️ Updated name

--- Hetzner/dns_lna-dev.net.tf
+++ Hetzner/dns_lna-dev.net.tf
@@ -66,8 +66,7 @@ resource "hetznerdns_record" "personalWebsiteBraveVerification" {
 }
 
 # Tutnaota
-
-resource "hetznerdns_record" "lnaDevNetTutanotaVerify" {
+resource "hetznerdns_record" "lna_dev_netTutanotaVerify" {
   zone_id = data.hetznerdns_zone.fedodo_org.id
   name    = "@"
   value   = "t-verify=78e08942b3e1ea847ee37d661afefbf9"
