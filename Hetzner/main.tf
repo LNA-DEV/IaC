bf6c2e4e40ae4f8b5a97a599fe9c93da67d72937
Lukas Nagel <lukas@lna-dev.net>
2024-09-22 16:09:44 +0200

    ⚗️ Removed version

--- Hetzner/main.tf
+++ Hetzner/main.tf
@@ -9,11 +9,9 @@ terraform {
   required_providers {
     hcloud = {
       source  = "hetznercloud/hcloud"
-      version = "1.35.2"
     }
     hetznerdns = {
       source  = "timohirt/hetznerdns"
-      version = "2.2.0"
     }
   }
 }
