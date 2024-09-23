73f96d464b5c80be3f55eab5d6d9eb1ebf33cfe7
Lukas Nagel <lukas@lna-dev.net>
2024-09-23 20:45:31 +0200

    Revert "⚗️ Removed version"
    This reverts commit bf6c2e4e40ae4f8b5a97a599fe9c93da67d72937.

--- Hetzner/main.tf
+++ Hetzner/main.tf
@@ -9,9 +9,11 @@ terraform {
   required_providers {
     hcloud = {
       source  = "hetznercloud/hcloud"
+      version = "1.35.2"
     }
     hetznerdns = {
       source  = "timohirt/hetznerdns"
+      version = "2.2.0"
     }
   }
 }
