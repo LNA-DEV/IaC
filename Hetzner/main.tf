cbfe78a925585e2eb1d07867b6c57550aaf266cf
Lukas Nagel <github@lna-dev.net>
2024-12-07 15:25:30 +0100

    🧱 Cluster v2

--- Hetzner/main.tf
+++ Hetzner/main.tf
@@ -2,26 +2,18 @@ terraform {
   cloud {
     organization = "LNA-DEV"
     workspaces {
-      name = "IaC_Hetzner"
+      name = "Hetzner_v2"
     }
   }
 
   required_providers {
     hcloud = {
       source  = "hetznercloud/hcloud"
-      version = "1.35.2"
-    }
-    hetznerdns = {
-      source  = "timohirt/hetznerdns"
-      version = "2.2.0"
+      version = "1.49.1"
     }
   }
 }
 
 provider "hcloud" {
   token = var.hcloud_token
-}
-
-provider "hetznerdns" {
-  apitoken = var.hcloud_dns_token
 }
